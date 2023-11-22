9494c22dc007f6da05cd88c48f9600f5461ad1a6
Thomas PUJOL <thomas.pujol@telecom-paris.fr>
2023-11-22 10:03:38 +0100

    Skeletton

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,5 +1,20 @@
-"""
-ToDo : Dataset class
-"""
+"""Dataset"""
+from pathlib import Path
+from typing import Any, Callable, Optional, Tuple
 
-class Dataset
+import PIL.Image
+
+from torchvision.datasets.utils import check_integrity, download_and_extract_archive, download_url, verify_str_arg
+from torchvision.datasets.vision import VisionDataset
+
+
+class Dataset(VisionDataset):
+
+    def __getitem__() -> Any:
+        """
+        Returns a tuple of (image, target) where target is index of the target class.
+        """
+        raise NotImplementedError
+    
+    def __len__(self) -> int:
+        raise NotImplementedError
