486f97db741b9b6fa930e2d8eb74bd063807ae57
Thomas PUJOL <thomas.pujol@telecom-paris.fr>
2023-11-23 10:32:21 +0100

    Bigger images

--- training/main.py
+++ training/main.py
@@ -13,7 +13,7 @@ model = Autoencoder()
 
 # Define transform
 transform = transforms.Compose([
-    transforms.Resize((128, 128)),
+    transforms.Resize((256, 256)),
     transforms.ToTensor(),
 ])
 
